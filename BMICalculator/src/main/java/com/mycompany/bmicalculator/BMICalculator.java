923bb884d352914bae3355a7e9601680a2b30db4
Matthew van Heerden <ST10508618@myemeris.edu.za>
2026-03-04 20:19:18 +0200

    Added BMICalculator project and imported Scanner

---
+++ BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bmicalculator;
+
+import java.util.Scanner;
+public class BMICalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
