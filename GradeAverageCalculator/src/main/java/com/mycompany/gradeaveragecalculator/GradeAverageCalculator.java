01a702414db36b74a333e46e06582a51640e056d
Matthew van Heerden <ST10508618@myemeris.edu.za>
2026-03-04 19:51:45 +0200

    Stored user input into variables.

--- GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
+++ GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
@@ -14,11 +14,13 @@ public class GradeAverageCalculator {
         
         // Ask user for input
         System.out.println("Enter score 1: ");
-        
+        score1 = input.nextInt();
         
         System.out.println("Enter score 2: ");
+        score2 = input.nextInt();
         
         System.out.println("Enter score 3: ");
+        score3 = input.nextInt();
         
         input.close();
     }
