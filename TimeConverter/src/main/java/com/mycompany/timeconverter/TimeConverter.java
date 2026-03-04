c888b82dbd83965073d512c1353b0b56fae9ee24
Matthew van Heerden <ST10508618@myemeris.edu.za>
2026-03-04 20:04:11 +0200

    Created TimeConverter and imported Scanner

---
+++ TimeConverter/src/main/java/com/mycompany/timeconverter/TimeConverter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timeconverter;
+
+import java.util.Scanner;
+public class TimeConverter {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+    }
+}
