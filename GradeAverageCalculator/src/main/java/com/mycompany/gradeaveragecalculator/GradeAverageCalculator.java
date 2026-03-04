64ab4ac1977049eca34ba890cc19342429316914
Matthew van Heerden <ST10508618@myemeris.edu.za>
2026-03-04 19:42:05 +0200

    Added files, imported Scanner and created Scanner object.
    Added GradeAverageCalculator project. Then imported Scanner class and created Scanner object (input) to get input from user.

---
+++ GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradeaveragecalculator;
+
+import java.util.Scanner;
+public class GradeAverageCalculator {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
